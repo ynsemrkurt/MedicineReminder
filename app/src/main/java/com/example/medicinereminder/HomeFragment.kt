6368a545ff5ed5535149d14b5864ca293dad6209
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-18 15:08:46 +0300

    feat: Add HomeFragment UI

---
+++ app/src/main/java/com/example/medicinereminder/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.medicinereminder
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.medicinereminder.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Medicine Fragment'ı Açmak için kullanılır
+        binding.buttonMedicine.setOnClickListener {
+            (activity as MainActivity).openFragment(MedicineFragment())
+        }
+
+        //Reminder Settings Fragment'ı Açmak için kullanılır
+        binding.buttonSettings.setOnClickListener {
+            (activity as MainActivity).openFragment(ReminderSettingsFragment())
+        }
+    }
+}
