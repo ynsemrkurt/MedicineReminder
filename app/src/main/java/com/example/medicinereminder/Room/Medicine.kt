82eefed0534814630d6061f8b5fccb84abec401d
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-18 15:10:05 +0300

    feat: Create Medicine entity for Room database

---
+++ app/src/main/java/com/example/medicinereminder/Room/Medicine.kt
@@ -0,0 +1,13 @@
+package com.example.medicinereminder.Room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "medicine_table")
+data class Medicine(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    val name: String,
+    val dosage: String,
+    val timeToTake: String
+)
