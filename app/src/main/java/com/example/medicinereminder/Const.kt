7b9161636bcf736289f012ad07ed1d511e7b1816
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-11-11 17:27:18 +0300

    feat: Add Const object for shared data keys

---
+++ app/src/main/java/com/example/medicinereminder/Const.kt
@@ -0,0 +1,8 @@
+package com.example.medicinereminder
+
+object Const {
+    const val MED_NAME = "medicineName"
+    const val MED_DOSAGE = "medicineDosage"
+    const val MED_ID = "medicineId"
+    const val MED_TIME = "medicineTime"
+}
